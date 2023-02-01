f7eabf697a621bc4699669a977d909328fb7b7bb
Gustavo Passos <theguspassos@gmail.com>
2023-02-01 13:58:55 -0800

    feat: add tests

--- dpk.test.ts
+++ dpk.test.ts
@@ -1,8 +1,76 @@
-const { deterministicPartitionKey } = require("./dpk");
+import { EventObj } from "./dpk";
+import { deterministicPartitionKey } from "./dpk";
+import { faker } from '@faker-js/faker';
+import crypto from "crypto";
 
 describe("deterministicPartitionKey", () => {
-  it("Returns the literal '0' when given no input", () => {
+  it("should return literal '0' when given no input", () => {
     const trivialKey = deterministicPartitionKey();
     expect(trivialKey).toBe("0");
   });
+
+  describe('partition key is string', () => {
+    it("should return partitionKey and less than max length", () => {
+      const event: EventObj = {
+        partitionKey: faker.datatype.string(256),
+      };
+
+      const trivialKey = deterministicPartitionKey(event);
+      expect(trivialKey).toBe(event.partitionKey);
+    })
+
+    it('should return partitionKey encrypted if exceeds max length', () => {
+      const event: EventObj = {
+        partitionKey: faker.datatype.string(257),
+      };
+
+      const trivialKey = deterministicPartitionKey(event);
+      const encrypted = crypto.createHash("sha3-512").update(event.partitionKey).digest("hex");
+
+      expect(trivialKey).toBe(encrypted);
+    })
+  })
+
+  describe('partition key is not string', () => {
+    it('should return partition key encrypted if exceeds max length', () => {
+      const event: EventObj = {
+        partitionKey: {
+          test: faker.datatype.string(257)
+        }
+      };
+
+      const trivialKey = deterministicPartitionKey(event);
+
+      const data = JSON.stringify(event.partitionKey);
+      const encrypted = crypto.createHash("sha3-512").update(data).digest("hex");
+
+      expect(trivialKey).toBe(encrypted);
+    })
+
+    it('should return partitionKey as JSON if less than max length', () => {
+      const event: EventObj = {
+        partitionKey: {
+          test: faker.random.word()
+        }
+      };
+
+      const trivialKey = deterministicPartitionKey(event);
+      const data = JSON.stringify(event.partitionKey);
+
+      expect(trivialKey).toBe(data);
+    })
+  })
+
+  it('should return event encrypted if has no partition key', () => {
+    const event = {
+      test: faker.random.word()
+    };
+
+    const trivialKey = deterministicPartitionKey(event);
+
+    const data = JSON.stringify(event);
+    const encrypted = crypto.createHash("sha3-512").update(data).digest("hex");
+
+    expect(trivialKey).toBe(encrypted);
+  })
 });
--- dpk.ts
+++ dpk.ts
@@ -1,9 +1,38 @@
 import crypto from "crypto";
 
-export const deterministicPartitionKey = (event: any) => {
+export interface EventObj {
+  partitionKey: any;
+}
+
+export const deterministicPartitionKey = (event?: EventObj | any): string => {
   const TRIVIAL_PARTITION_KEY = "0";
   const MAX_PARTITION_KEY_LENGTH = 256;
-  let candidate;
+
+  let candidate: string | any;
+
+  // const getCandidateInEvent = (event?: EventObj): any => {
+  //   if (event && !event.partitionKey) {
+  //     const data = JSON.stringify(event);
+  //     return crypto.createHash("sha3-512").update(data).digest("hex");
+  //   }
+
+  //   return event?.partitionKey;
+  // }
+
+  // const candidateToString = (candidate: any): string => { 
+  //   if (typeof candidate !== "string")
+  //     return JSON.stringify(candidate);
+
+  //   return candidate ?? TRIVIAL_PARTITION_KEY;
+  // }
+
+  // const fixCandidateToMaxLength = (candidate: string): string => {
+  //   if (candidate.length > MAX_PARTITION_KEY_LENGTH) {
+  //     return crypto.createHash("sha3-512").update(candidate).digest("hex");
+  //   }
+
+  //   return candidate;
+  // }
 
   if (event) {
     if (event.partitionKey) {
